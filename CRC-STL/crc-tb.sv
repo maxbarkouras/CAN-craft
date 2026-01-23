4194db9ba047f27df7bfa7ea99371fd8be465d71
maxbarkouras <max@computer>
2026-01-22 17:23:10 -0800

    updates to sv code, aiming to replicate can crafter in hdl

--- CRC-STL/crc-tb.sv
+++ CRC-STL/crc-tb.sv
@@ -3,17 +3,23 @@ module crc_tb;
     timeprecision 1ps;
 
     bit bit_in;
-    bit clk, rst = 0, init = 0, enable = 0;
-    bit [7:0] crc;
-    reg [7:0] data; 
+    bit clk, rst = 0, init = 0, data_en = 0, id_en = 0;
+    bit [63:0] data_recv;
+    bit [10:0] id_recv;
+    bit [7:0] data; 
+    bit [10:0] can_id;
+    bit [7:0] can_data [0:7];
+    bit [2:0] data_bytes;
 
     crc_create maker(
         .clk(clk),
         .rst(rst),
         .bit_in(bit_in),
         .init(init),
-        .enable(enable),
-        .crc(crc)
+        .data_en(data_en),
+        .packet(data_recv),
+        .id_en(id_en),
+        .can_id(id_recv)
     );
 
     always begin
@@ -23,25 +29,48 @@ module crc_tb;
 	 
 	initial begin
 
-        data = 8'hA2;
+        data_bytes = 3'd3;
+        //can_data = 0;
+        can_data[0] = 8'h10;
+        can_data[1] = 8'h02;
+        can_data[2] = 8'h01;
+        can_id = 11'h7ff;
 
         init = 1;
         @(posedge clk);
         init = 0;
 
-        enable = 1;
-        for(int i=0;i<8;i++) begin
+        id_en = 1;
+        for(int i=11; i>0; i--) begin
+
             @(negedge clk);
-            bit_in = data[7-i];
+            bit_in = can_id[i-1];
             @(posedge clk);
-            //$display("%d %b", bit_in, crc);
+            $display("sending: %b", bit_in);
 
         end
 
-        enable = 0;
+        id_en = 0;
         @(posedge clk);
-        $display("complete: 0x%h", crc);
-        $stop;
+        $display("id recieved: 0x%x",id_recv);
+
+        data_en = 1;
+        for(int i=data_bytes; i>0; i--) begin
+            for(int j=0; j<8; j++) begin
+
+                @(negedge clk);
+                bit_in = can_data[data_bytes-i][7-j];
+                @(posedge clk);
+                $display("sending: %b", bit_in);
+
+            end
+            $display("sent: 0x%x", can_data[data_bytes-i]);
+        end
+
+        data_en = 0;
+        @(posedge clk);
+        $display("data recieved: 0x%x",data_recv);
+        $finish;
 
     end
 		 
--- CRC-STL/crc.sv
+++ CRC-STL/crc.sv
@@ -3,21 +3,28 @@ module crc_create(
     input logic clk,
     input logic rst,
     input logic init,
-    input logic enable,
-
+    input logic data_en,
     input logic bit_in,
+    input logic id_en,
 
-	input logic crc_next,
-	output logic [7:0] crc
+    output logic [63:0] packet,
+    output logic [10:0] can_id
 
-    // output logic [14:0] crc;
 );
 
     always_ff @(posedge clk) begin
-        if (init)
-            crc <= '0;
-        else if (enable)
-            crc <= {crc[6:0], bit_in};
+        if (init) begin
+            packet <= '0;
+            can_id <= '0;
+        end
+        else if (rst) begin
+            packet <= '0;
+            can_id <= '0;
+        end
+        else if (id_en)
+            can_id <= {can_id[9:0], bit_in};
+        else if (data_en)
+            packet <= {packet[22:0], bit_in};
     end
 
 endmodule
